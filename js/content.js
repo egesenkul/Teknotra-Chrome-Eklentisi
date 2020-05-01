2453613505f8f65a1cd7f9509e9ef2a9b2ca85af
egesenkul <egesenkul35@hotmail.com>
2020-05-01 16:24:33 +0300

    Yazılar yükleniyorken ekranda gösterilecek yükleniyor animasyonu eklendi

--- js/content.js
+++ js/content.js
@@ -1,11 +1,12 @@
 $(document).ready(() => {
-    $('.news section').addClass('loading');
+    document.getElementById("logo").style.display = "block";
+    document.getElementById("yazilar").style.display = "none";
     $.getJSON({ url: "https://teknotra.com/wp-json/wp/v2/posts?per_page=3" }).done(data => {
         if (data) {
             $(".news #ilkYaziResmi").src= getImageForPost("ilkYaziResmi","https://teknotra.com/wp-json/wp/v2/media/"+data[0].featured_media,function(err, data) {});
             $(".news #ikinciYaziResmi").src= getImageForPost("ikinciYaziResmi","https://teknotra.com/wp-json/wp/v2/media/"+data[1].featured_media,function(err, data) {});
             $(".news #ucuncuYaziResmi").src= getImageForPost("ucuncuYaziResmi","https://teknotra.com/wp-json/wp/v2/media/"+data[2].featured_media,function(err, data) {});
-
+            ucuncuYaziResimLinki = "https://teknotra.com/wp-json/wp/v2/media/"+data[2].featured_media;
             if(data[0].title.rendered.length>57)
             {
                 $(".news #ilkYaziBaslik").text(data[0].title.rendered.substring(0,60).replace("&#8211;","-")+"...");
@@ -37,10 +38,12 @@ $(document).ready(() => {
     });
 });
 
+var ucuncuYaziResimLinki = null;
+
 var getImageForPost = function(elementName, url, callback) {
 
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
+    xhr.open('GET', url);
     xhr.responseType = 'json';
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onload = function() {
@@ -49,6 +52,10 @@ var getImageForPost = function(elementName, url, callback) {
         
         if (status == 200) {
             callback(null, xhr.response);
+            if(ucuncuYaziResimLinki != null && url == ucuncuYaziResimLinki){
+                document.getElementById("yazilar").style.display = "block";
+                document.getElementById("logo").style.display = "none";
+            }
             document.getElementById(elementName).src = xhr.response.link;
         } else {
             callback(status);
